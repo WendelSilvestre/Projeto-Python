056d1535e441aa961b66885da74d1132b6fb75c2
Wendel De O. Silvestre <65190276+WendelSilvestre@users.noreply.github.com>
2022-10-04 10:51:21 -0300

    ADD automação inicial de enviar email

---
+++ EmailAutomation.py
@@ -0,0 +1,86 @@
+import pyautogui
+import pyperclip
+import time
+import pandas as pd
+
+pyautogui.PAUSE = 1
+
+# pyautogui.click -> clicar
+# pyautogui.press -> apertar 1 tecla
+# pyautogui.hotkey -> conjunto de teclas
+# pyautogui.write -> escreve um texto
+
+
+pyautogui.hotkey("ctrl", "t")
+pyperclip.copy("https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga?usp=sharing")
+pyautogui.hotkey("ctrl", "v")
+pyautogui.press("enter")
+
+time.sleep(5)
+
+# Passo 2: Navegar no sistema e encontrar a base de vendas 
+# Descobrir a posição do click pyautgui.position()
+pyautogui.click(x=1023, y=710, clicks=2)
+time.sleep(2)
+
+# Passo 3: Fazer o download da base de vendas
+pyautogui.click(x=1100, y=904) # clicar no arquivo
+pyautogui.click(x=3288, y=411) # clicar nos 3 pontinhos
+pyautogui.click(x=2716, y=1523) # clicar no fazer download
+time.sleep(5) # esperar o download acabar
+
+
+# Passo 4: Importar a base de vendas pro Python
+tabela = pd.read_excel(r"C:\Users\wendel\Downloads")
+display(tabela)
+
+
+# Passo 5: Calcular os indicadores da empresa
+faturamento = tabela["Valor Final"].sum()
+print(faturamento)
+quantidade = tabela["Quantidade"].sum()
+print(quantidade)
+
+# Passo 6: Enviar um e-mail para a diretoria com os indicadores de venda
+
+# abrir aba
+pyautogui.hotkey("ctrl", "t")
+
+# entrar no link do email - https://mail.google.com/mail/u/0/#inbox
+pyperclip.copy("https://mail.google.com/mail/u/0/#inbox")
+pyautogui.hotkey("ctrl", "v")
+pyautogui.press("enter")
+time.sleep(5)
+
+# clicar no botão escrever
+pyautogui.click(x=240, y=415)
+
+# preencher as informações do e-mail
+pyautogui.write("empresa@gmail.com")
+pyautogui.press("tab") # selecionar o email
+
+pyautogui.press("tab") # pular para o campo de assunto
+pyperclip.copy("Relatório de Vendas")
+pyautogui.hotkey("ctrl", "v")
+
+pyautogui.press("tab") # pular para o campo de corpo do email
+
+texto = f"""
+Prezados,
+
+Segue relatório de vendas.
+Faturamento: R${faturamento:,.2f}
+Quantidade de produtos vendidos: {quantidade:,}
+
+Qualquer dúvida estou à disposição.
+Att,
+Wendel De Oliveira Silvestre
+"""
+
+# formatação dos números (moeda, dinheiro)
+
+pyperclip.copy(texto)
+pyautogui.hotkey("ctrl", "v")
+
+# enviar o e-mail
+pyautogui.hotkey("ctrl", "enter")
